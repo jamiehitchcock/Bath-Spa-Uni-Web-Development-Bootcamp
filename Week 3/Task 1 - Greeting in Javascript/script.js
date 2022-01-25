0e8bf378cbe6750654b75952c9d3fc18e0ef2b2c
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-01-25 21:15:45 +0000

    Update script.js

--- Week 3/Task 1 - Greeting in Javascript/script.js
+++ Week 3/Task 1 - Greeting in Javascript/script.js
@@ -1,6 +1,6 @@
 //function to greet user
 function greetName() {
-    name = prompt('Enter your name');
+    let name = prompt('Enter your name');
     alert("Hello " + name.charAt(0).toUpperCase() + name.slice(1) + "!");
     };
 greetName();
