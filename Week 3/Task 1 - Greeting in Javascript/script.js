12453e804162a9d3c074d471278d36856d06f176
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-01-25 21:08:11 +0000

    Week 3 Task 1 - Added variable swapping
    Console log shows variable swapping, using destructuring from ES6.

--- Week 3/Task 1 - Greeting in Javascript/script.js
+++ Week 3/Task 1 - Greeting in Javascript/script.js
@@ -1,5 +1,13 @@
+//function to greet user
 function greetName() {
     name = prompt('Enter your name');
     alert("Hello " + name.charAt(0).toUpperCase() + name.slice(1) + "!");
     };
 greetName();
+
+let a = 3;
+let b = 8;
+console.log(`A = ${a}, B = ${b}`);
+//Destructuring in ES6 to swap variables.
+[a, b] = [b, a];
+console.log(`A = ${a}, B = ${b}`);
