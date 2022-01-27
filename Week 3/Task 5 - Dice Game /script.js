de010295798bb232ff6daa114bcf84869bfefe23
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-01-27 22:00:52 +0000

    Week 3 Task 5 Dice Game Completion
    Completed the calculation of winner. Matching player  1 and 2 scores together, then updating the result.

--- Week 3/Task 5 - Dice Game /script.js
+++ Week 3/Task 5 - Dice Game /script.js
@@ -1,83 +1,63 @@
 
 //Assign the images in the DOM
+let result = document.getElementById("result");
 let die1Image = document.getElementById("die1");
 let die2Image = document.getElementById("die2");
 
-function play(){
+//function to play the game
+function play() {
 
+    //role die 1
+    let player1Num = Math.floor(Math.random() * 6) + 1;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// calculate winner
-let result = document.getElementById("result");
-if (player1Num > player2Num){
-    result.innerHTML = "Player 1 Wins !"
-}
-else if (player1Num < player2Num){
-    result.innerHTML = "Player 2 Wins !"
-}
-else {
-    result.innerHTML = "Draw !"
-};
-}
-
-//Roll player 1 die and change images
-function rollDie1(){
-    let player1Num = Math.floor(Math.random()*6)+1;
-
-    switch(player1Num){
-        case 1: die1Image.src="images/dice1.png";
-        break;
-        case 2: die1Image.src="images/dice2.png";
-        break;
-        case 3: die1Image.src="images/dice3.png";
-        break;
-        case 4: die1Image.src="images/dice4.png";
-        break;
-        case 5: die1Image.src="images/dice5.png";
-        break;
-        case 6: die1Image.src="images/dice6.png";
-        break;
+    //change die 1 iamge to match the score
+    switch (player1Num) {
+        case 1: die1Image.src = "images/dice1.png";
+            break;
+        case 2: die1Image.src = "images/dice2.png";
+            break;
+        case 3: die1Image.src = "images/dice3.png";
+            break;
+        case 4: die1Image.src = "images/dice4.png";
+            break;
+        case 5: die1Image.src = "images/dice5.png";
+            break;
+        case 6: die1Image.src = "images/dice6.png";
+            break;
     }
-}
 
-//Roll player 2 die and change images
-function rollDie2(){
-    let player2Num = Math.floor(Math.random()*6)+1;
+    //role die 2
+    let player2Num = Math.floor(Math.random() * 6) + 1;
 
-    switch(player2Num){
-        case 1: die2Image.src="images/dice1.png";
-        break;
-        case 2: die2Image.src="images/dice2.png";
-        break;
-        case 3: die2Image.src="images/dice3.png";
-        break;
-        case 4: die2Image.src="images/dice4.png";
-        break;
-        case 5: die2Image.src="images/dice5.png";
-        break;
-        case 6: die2Image.src="images/dice6.png";
-        break;
+    //change die 2 iamge to match the score
+    switch (player2Num) {
+        case 1: die2Image.src = "images/dice1.png";
+            break;
+        case 2: die2Image.src = "images/dice2.png";
+            break;
+        case 3: die2Image.src = "images/dice3.png";
+            break;
+        case 4: die2Image.src = "images/dice4.png";
+            break;
+        case 5: die2Image.src = "images/dice5.png";
+            break;
+        case 6: die2Image.src = "images/dice6.png";
+            break;
     }
+
+    // calculate and display winner
+    if (player1Num > player2Num) {
+        result.innerHTML = "Player 1 Wins !"
+    }
+    else if (player1Num < player2Num) {
+        result.innerHTML = "Player 2 Wins !"
+    }
+    else {
+        result.innerHTML = "Draw !"
+    };
 }
 
 //Execute die roll functions on loading the page
 document.addEventListener("DOMContentLoaded", function () {
-    rollDie1();
-    rollDie2();
+    play();
 });
-
-
