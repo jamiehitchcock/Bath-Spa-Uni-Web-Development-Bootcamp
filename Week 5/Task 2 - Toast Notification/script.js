86623240080eac47b5ac6a655e8c25688cbdc941
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-07 20:09:23 +0000

    W5 T2 Toast Notification Initial Commit
    Initial Commit

---
+++ Week 5/Task 2 - Toast Notification/script.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("btn");
+const container = document.getElementById("container");
+
+
+btn.addEventListener("click",()=>{
+    createNotification();
+});
+
+function createNotification(){
+    const note = document.createElement("div");
+    note.classList.add("toast");
+    note.innerText="This was made on a web development bootcamp";
+    container.appendChild(note);
+    setTimeout(()=>{
+        note.remove()},3000);
+}
+
+
+
+
+
