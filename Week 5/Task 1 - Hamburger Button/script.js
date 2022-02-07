0c6024e818f1087d98f574c4c353d7e5e69adf35
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-07 21:42:49 +0000

    W5 T2 - Hamburger Icon Additional Icon
    Added an additional icon for the close, alongside traditional hamburger icon to open. Function to swap the two icons out when button is clicked.

--- Week 5/Task 1 - Hamburger Button/script.js
+++ Week 5/Task 1 - Hamburger Button/script.js
@@ -1,7 +1,31 @@
-const btn = document.getElementById('btn');
-const nav = document.getElementById('nav');
+// apply variables to each dom element
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".closeIcon");
 
-btn.addEventListener("click", () => {
-    btn.classList.toggle("active");
-    nav.classList.toggle("active");
-});
+// function to toggle the menu
+function toggleMenu() {
+    // if the menu is appearing then hide it and change button to menuIcon
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else { 
+        // otherwise show menu and change button to closeIcon
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+//   execute the toggleMenu function when the hamburger icons are clicked on
+hamburger.addEventListener("click", toggleMenu);
+
+// execute the toggleMenu function when an item is clicked on, to hide the menu
+menuItems.forEach(
+    function (menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
