b5d1f915baecf575731936427ee995babe81c7e3
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-08 20:39:10 +0000

    Week 5 Task 5 Background Changer Initial Commit
    Background colour that changes by generating a random rgb value upon clicking the button. Also added innerHTML to display these rgb values.

---
+++ Week 5/Task 5 - Background Changer/script.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#btn");
+const rgb = document.querySelector("#rgb");
+
+function randomBackground(){
+   let a = Math.floor(Math.random() * 255) + 1;
+   let b = Math.floor(Math.random() * 255) + 1;
+   let c = Math.floor(Math.random() * 255) + 1;
+   let backgroundNumber = `rgb(${a}, ${b}, ${c})`;
+   document.body.style.backgroundColor = backgroundNumber;
+   rgb.innerHTML = backgroundNumber;
+}
+
+btn.addEventListener("click", (e) => {
+    randomBackground();
+});
