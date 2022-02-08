ab2010da21f06308fd818e6af1e32eaad72f71dc
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-08 20:56:05 +0000

    Update script.js
    Corrected random number as rgb begins at 0 not 1.

--- Week 5/Task 5 - Background Changer/script.js
+++ Week 5/Task 5 - Background Changer/script.js
@@ -2,9 +2,9 @@ const btn = document.querySelector("#btn");
 const rgb = document.querySelector("#rgb");
 
 function randomBackground(){
-   let a = Math.floor(Math.random() * 255) + 1;
-   let b = Math.floor(Math.random() * 255) + 1;
-   let c = Math.floor(Math.random() * 255) + 1;
+   let a = Math.floor(Math.random() * 255);
+   let b = Math.floor(Math.random() * 255);
+   let c = Math.floor(Math.random() * 255);
    let backgroundNumber = `rgb(${a}, ${b}, ${c})`;
    document.body.style.backgroundColor = backgroundNumber;
    rgb.innerHTML = backgroundNumber;
