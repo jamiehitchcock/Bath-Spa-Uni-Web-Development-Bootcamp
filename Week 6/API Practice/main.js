f31eaf8f4ac48eccdf1381b8740576801794ac74
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2022-02-24 19:56:31 +0000

    Star Wars API Practice Initial Commit

--- Week 6/API Practice/main.js
+++ Week 6/API Practice/main.js
@@ -1,10 +1,38 @@
-const apiUrl = "https://reqres.in/api/users?page=2";
+const url = "https://swapi.dev/api/planets";
 
-fetch (apiUrl, {
-    method: 'GET'
+fetch(url)
+.then((response) => {
+        return response.json()
+    })
+.then((data) => {
+    const planets = (data.results);
+    planets.map((planet) => {
+        createPlanetDiv(planet);
+        console.log(planet);
+    })
+    
 })
-.then(response => response.json())
-.then(data => {console.log(data);})
 .catch((error) => console.log(error.message));
 
-let demo = document.getElementById("demo");
+function createPlanetDiv(planet){
+    const target = document.getElementById("target")
+    const div = document.createElement("div");
+    div.classList.add()
+    const h3Name = document.createElement("h3");
+    const pName = document.createElement("h3");
+    pName.innerHTML = planet.name;
+    const h3Pop = document.createElement("h3");
+    const pPop = document.createElement("h3");
+    pPop.innerHTML = planet.population;
+    const h3terr = document.createElement("h3");
+    const pTerr = document.createElement("h3");
+    pTerr.innerHTML = planet.terrain;
+
+
+    div.appendChild(pName);
+    div.appendChild(pPop);
+    div.appendChild(pTerr);
+
+    target.appendChild(div);
+}
+
